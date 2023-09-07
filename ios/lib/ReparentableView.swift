3f3e7452b7765549881628c932c880a980800387
Michael Gerdes <mgerdes11@gmail.com>
2023-09-07 15:12:34 -0400

    update

--- ios/lib/ReparentableView.swift
+++ ios/lib/ReparentableView.swift
@@ -5,6 +5,7 @@ protocol PortalView { }
 
 extension PortalView where Self: UIView {
   func moveTo(destination: UIView) {
+    destination.subviews.forEach({ $0.removeFromSuperview() })
     subviews.forEach({ destination.addSubview($0) })
   }
 }
